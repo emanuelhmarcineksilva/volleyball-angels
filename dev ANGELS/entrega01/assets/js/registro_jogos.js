f4a6cfddac3c6f490adaf6cf571bd5cc7fd35d4b
EmillyB-O <emyy.cursos@gmail.com>
2025-10-07 11:43:59 -0300

    Criei arquivo html da criação de jogo, css e js

---
+++ dev ANGELS/entrega01/assets/js/registro_jogos.js
@@ -0,0 +1,20 @@
+document.getElementById('enviar_jogo').addEventListener('click', function(){
+    armazenarJogo();
+    window.location.href = 'registro_jogos.html';
+});
+
+function armazenarJogo(){
+    var listaJogos = JSON.parse(localStorage.getItem('listaJogos'));
+    if (!listaJogos) {
+        var listaJogos = [];
+    }
+    var jogo = {inicio: "", fim: "", duracao: "", adversario: "", tipo: "", pontClube: "", pontAdvers: "", observacoes: ""};
+    jogo.inicio = document.getElementById('inicio_jogo').value;
+    jogo.fim = document.getElementById('fim_jogo').value;
+    
+    var inicio = new Date(jogo.inicio);
+    var fim = new Date(jogo.fim);
+    var duracao = (fim - inicio) / 1000; // Duracao em segundos
+
+
+}
